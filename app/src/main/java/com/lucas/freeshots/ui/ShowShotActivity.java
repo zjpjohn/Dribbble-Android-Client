71f66f874011457baacc9c39e1b1caaa7d250f52
LucasJia <jiayangcg@hotmail.com>
2016-02-15 01:03:37 +0800

    完善显示一个shot的Activity

--- app/src/main/java/com/lucas/freeshots/ui/ShowShotActivity.java
+++ app/src/main/java/com/lucas/freeshots/ui/ShowShotActivity.java
@@ -3,15 +3,13 @@ package com.lucas.freeshots.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Animatable;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.text.Html;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,10 +19,11 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.controller.BaseControllerListener;
-import com.facebook.drawee.controller.ControllerListener;
 import com.facebook.drawee.view.SimpleDraweeView;
-import com.facebook.imagepipeline.image.ImageInfo;
+import com.facebook.imagepipeline.request.BasePostprocessor;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.facebook.imagepipeline.request.Postprocessor;
 import com.lucas.freeshots.R;
 import com.lucas.freeshots.model.Shot;
 
@@ -54,7 +53,9 @@ public class ShowShotActivity extends AppCompatActivity {
 //        toolbar.setNavigationOnClickListener(view -> finish());
 
         LinearLayout topBar = $(this, R.id.top_bar);
+        SimpleDraweeView topBarAuthorIconDv = $(this, R.id.top_bar_author_icon);
         TextView topBarShotTitleTv = $(this, R.id.top_bar_shot_title);
+        TextView topBarAuthorNameTv = $(this, R.id.top_bar_author_name);
         View backV = $(this, R.id.back);
         backV.setOnClickListener(view -> finish());
         ViewGroup topBarInfoZoneVg = $(this, R.id.top_bar_info_zone);
@@ -62,10 +63,13 @@ public class ShowShotActivity extends AppCompatActivity {
         AppBarLayout appBarLayout = $(this, R.id.app_bar);
         CollapsingToolbarLayout toolbarLayout = $(this, R.id.toolbar_layout);
         SimpleDraweeView shotDv = $(this, R.id.shot);
+
+        View titleAuthorZone = $(this, R.id.title_author_zone);
         SimpleDraweeView authorIconDv = $(this, R.id.author_icon);
         TextView shotTitleTv = $(this, R.id.shot_title);
         TextView authorNameTv = $(this, R.id.author_name);
 
+        View shotInfoZone = $(this, R.id.shot_info_zone);
         TextView likesCountTv = $(this, R.id.likes_count);
         TextView commentsCountTv = $(this, R.id.comments_count);
         TextView bucketsCountTv = $(this, R.id.buckets_count);
@@ -98,67 +102,70 @@ public class ShowShotActivity extends AppCompatActivity {
                                 + "  topBarHeight:" + topBarHeight
                 );
 
-                if(verticalOffset == 0) {
-                    return;
-                }
+//                if (verticalOffset == 0) {
+//                    return;
+//                }
 
-                topBar.setAlpha(Math.abs(verticalOffset / (float)appBarLayoutHeight));
+                topBar.setAlpha(Math.abs(verticalOffset / (float) appBarLayoutHeight));
 
                 int tmp = appBarLayoutHeight + verticalOffset;
-                if (tmp <= topBarHeight) {
-                    topBarInfoZoneVg.setAlpha(1 - Math.abs(tmp / (float)topBarHeight));
+                if (tmp < topBarHeight) {
+                    topBarInfoZoneVg.setAlpha(1 - Math.abs(tmp / (float) topBarHeight));
                 } else {
                     topBarInfoZoneVg.setAlpha(0);
                 }
             }
         });
 
-//        Palette palette = Palette.from(new Bitmap()).generate(new Palette.PaletteAsyncListener() {
-//            @Override
-//            public void onGenerated(Palette palette) {
-//                Palette.Swatch vibrant = palette.getVibrantSwatch();
-//                Palette.Swatch darkVibrant = palette.getDarkVibrantSwatch();
-//                vibrant.getBodyTextColor();
-//            }
-//        });
-
-        ControllerListener controllerListener = new BaseControllerListener<ImageInfo>() {
+        Postprocessor postprocessor = new BasePostprocessor() {
             @Override
-            public void onFinalImageSet(String id, ImageInfo imageInfo, Animatable animatable) {
-                super.onFinalImageSet(id, imageInfo, animatable);
+            public void process(Bitmap bitmap) {
+                super.process(bitmap);
 
-                Timber.e("111111111111111111111111111111111111111111111");
-                Drawable drawable = shotDv.getDrawable();
-                //drawable 是 GenericDraweeHierarchy.RootDrawable;
+                Palette.from(bitmap).generate(palette -> {
+                    Palette.Swatch vibrant = palette.getVibrantSwatch();
+                    Palette.Swatch darkVibrant = palette.getDarkVibrantSwatch();
+                    //vibrant.getBodyTextColor();
+                    if(vibrant != null && darkVibrant != null) {
+                        int vibrantRgb = vibrant.getRgb();
+                        int darkVibrantRgb = darkVibrant.getRgb();
 
-                if (drawable != null && drawable instanceof BitmapDrawable) {
-                    Timber.e("2222222222222222222222222222222222222222222222");
-                    Bitmap bItmap = ((BitmapDrawable) drawable).getBitmap();
-                }
+                        titleAuthorZone.setBackgroundColor(darkVibrantRgb);
+                        shotInfoZone.setBackgroundColor(vibrantRgb);
+
+                        topBar.setBackgroundColor(darkVibrantRgb);
+                        topBarInfoZoneVg.setBackgroundColor(darkVibrantRgb);
+                    }
+                });
             }
         };
 
         if(shot.images != null) {
             String uri = shot.images.getHeightImageUri();
             if(uri != null) {
+                ImageRequest request = ImageRequestBuilder
+                                    .newBuilderWithSource(Uri.parse(uri))
+                                    .setPostprocessor(postprocessor)
+                                    .build();
+
                 shotDv.setController(Fresco.newDraweeControllerBuilder()
-                        .setUri(uri)
+                        //.setUri(uri)
                         .setAutoPlayAnimations(true)
-                        .setControllerListener(controllerListener)
+                        .setOldController(shotDv.getController())
+                        .setImageRequest(request)
                         .build());
                // shotDv.getDrawable();
             }
         }
 
         shotTitleTv.setText(shot.title + " : " + shot.images.getType() + " : " + shot.width + ", " + shot.height);
-        if(shot.description != null) {
-            shotDescribeTv.setText(Html.fromHtml(shot.description));
-        }
 
-        likesCountTv.setText(String.valueOf(shot.likes_count));
-        commentsCountTv.setText(String.valueOf(shot.comments_count));
-        bucketsCountTv.setText(String.valueOf(shot.buckets_count));
-        viewsCountTv.setText(String.valueOf(shot.views_count));
+        shotDescribeTv.setText(shot.description != null ? Html.fromHtml(shot.description) : "null");
+
+        likesCountTv.setText(String.format("%d likes", shot.likes_count));
+        commentsCountTv.setText(String.format("%d comments", shot.comments_count));
+        bucketsCountTv.setText(String.format("%d buckets", shot.buckets_count));
+        viewsCountTv.setText(String.format("%d views", shot.views_count));
 
         for(String tag : shot.tags) {
             Button b = new Button(this);
@@ -168,12 +175,15 @@ public class ShowShotActivity extends AppCompatActivity {
 
         if(shot.user != null) {
             if(shot.user.avatar_url != null) {
-                authorIconDv.setImageURI(Uri.parse(shot.user.avatar_url));
+                Uri uri = Uri.parse(shot.user.avatar_url);
+                authorIconDv.setImageURI(uri);
+                topBarAuthorIconDv.setImageURI(uri);
             }
 
             String s = String.format("by %s, %s",
                     String.valueOf(shot.user.name), String.valueOf(shot.user.updated_at));
             authorNameTv.setText(s);
+            topBarAuthorNameTv.setText(s);
         }
 
         commentIv.setOnClickListener((view) -> {
